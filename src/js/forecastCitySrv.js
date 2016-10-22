4b84cb6797db23c1f91783515d28c95631eb544d
Smirnov Mikhail <urine89@mail.ru>
2016-10-22 21:26:03 +0300

    Add geocoder to service and console.log the city name

--- src/js/forecastCitySrv.js
+++ src/js/forecastCitySrv.js
@@ -8,6 +8,33 @@
 
     this.getCity = function(lat, lng) {
 
+      // here we add reverse geocoding because openWeatherMap doesn't show city by my positioning
+      // it does show station with name 'Kalininskoe', so we convert coordinates to city name
+      // by google maps
+      function getReverseGeocodingData(lat, lng) {
+          var latlng = new google.maps.LatLng(lat, lng);
+          var geocoder = new google.maps.Geocoder();
+
+          geocoder.geocode({ 'latLng': latlng }, function (results, status) {
+              if (status !== google.maps.GeocoderStatus.OK) {
+                  alert(status);
+              }
+              if (status == google.maps.GeocoderStatus.OK) {
+                  console.log(results);
+                  var arrAddress = results[0].address_components;
+
+                  for (var i = 0; i < arrAddress.length; i++) {
+                    if (arrAddress[i].types[0] == "locality") {
+                      console.log(arrAddress[i].long_name); // City name - now we should find how to use Eng language
+                      return false;
+                    }
+                  }
+              }
+          });
+      }
+
+      getReverseGeocodingData(lat, lng);
+
       var API_KEY = window.localStorage.getItem('API_KEY');
       $http({
         method: 'GET',
@@ -17,7 +44,7 @@
         }
       }).then(function(response) {
         var data = response.data;
-        console.log(data);
+        // console.log(data);
       });
 
     };
--- src/js/forecastCtrl.js
+++ src/js/forecastCtrl.js
@@ -7,19 +7,18 @@ function forecastCtrl($scope, $getCitySrv) {
 
   $scope.getPosition = function() {
     if (navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition(successFunction);
+      navigator.geolocation.getCurrentPosition(successFunction);
     } else {
-        alert('It seems like Geolocation, which is required for this page, is not enabled in your browser. Please use a browser which supports it.');
+      alert('It seems like Geolocation, which is required for this page, is not enabled in your browser. Please use a browser which supports it.');
     }
   }
 
   $scope.getPosition();
 
   function successFunction(position) {
-      var lat = position.coords.latitude;
-      var lng = position.coords.longitude;
-
-      $getCitySrv.getCity(lat, lng);
+    var lat = position.coords.latitude;
+    var lng = position.coords.longitude;
+    $getCitySrv.getCity(lat, lng);
   }
 
 };
