713a68045041969edbd5e65e79b3770abb1f9638
Smirnov Mikhail <urine89@mail.ru>
2016-10-23 19:21:48 +0300

    Add download forecast locally function

--- src/js/forecastCtrl.js
+++ src/js/forecastCtrl.js
@@ -81,4 +81,23 @@ function forecastCtrl($scope, $getWeatherSrv, $q) {
     });
   }
 
+  $scope.downloadForecast = function(weather, city) {
+    delete weather.icon;
+
+    var forecast = 'City: ' + city + '; situation: ' + weather.situation + '; temp: ' + weather.temp + '; wind speed: ' + 
+    weather.wind + ';';
+
+    var filename = 'forecast';
+    var element = document.createElement('a');
+    element.setAttribute('href', 'data:text/plain;charset=utf-8,' + forecast);
+    element.setAttribute('download', filename);
+
+    element.style.display = 'none';
+    document.body.appendChild(element);
+
+    element.click();
+
+    document.body.removeChild(element);
+  }
+
 };
